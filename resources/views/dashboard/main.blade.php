2aba7096f4a9c54c886519e5ca1d7ccf1e48e581
irhamrizaldy <43570194+irhamrizaldy@users.noreply.github.com>
2022-04-21 15:15:22 +0700

    initial commit // login using seeders

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -2,296 +2,78 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="/admin/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="/admin/img/favicon.png">
-    <title>
-        {{ $title }}
-    </title>
-    <!--     Fonts and icons     -->
-    <link rel="stylesheet" type="text/css"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-    <!-- Nucleo Icons -->
-    <link href="/admin/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="/admin/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- Font Awesome Icons Old & New-->
-    {{-- <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script> --}}
-    <script src="https://kit.fontawesome.com/2b329cdf00.js" crossorigin="anonymous"></script>
-    <!-- Material Icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-    <!-- CSS Files -->
-    <link id="pagestyle" href="/admin/css/material-dashboard.css?v=3.0.0" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <title>Admincast bootstrap 4 &amp; angular 5 admin template, Шаблон админки | Dashboard</title>
+    <!-- GLOBAL MAINLY STYLES-->
+    <link href="/admin/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/admin/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="/admin/vendors/themify-icons/css/themify-icons.css" rel="stylesheet" />
+    <!-- PLUGINS STYLES-->
+    <link href="/admin/vendors/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
+    <!-- THEME STYLES-->
+    <link href="/admin/css/main.min.css" rel="stylesheet" />
+    <!-- PAGE LEVEL STYLES-->
 </head>
 
-<body class="g-sidenav-show  bg-gray-200">
-    @yield('container')
-    <!--   Core JS Files   -->
-    <script src="/admin/js/core/popper.min.js"></script>
-    <script src="/admin/js/core/bootstrap.min.js"></script>
-    <script src="/admin/js/plugins/perfect-scrollbar.min.js"></script>
-    <script src="/admin/js/plugins/smooth-scrollbar.min.js"></script>
-    <script src="/admin/js/plugins/chartjs.min.js"></script>
-    <script>
-        var ctx = document.getElementById("chart-bars").getContext("2d");
+<body class="fixed-navbar">
+    <div class="page-wrapper">
+        @yield('container')
+    </div>
+    <!-- BEGIN THEME CONFIG PANEL-->
+    <div class="theme-config">
+        <div class="theme-config-toggle"><i class="fa fa-cog theme-config-show"></i><i
+                class="ti-close theme-config-close"></i>
+        </div>
+        <div class="theme-config-box">
+            <div class="text-center font-18 m-b-20">SETTINGS</div>
+            <div class="font-strong">LAYOUT OPTIONS</div>
+            <div class="check-list m-b-20 m-t-10">
+                <label class="ui-checkbox ui-checkbox-gray">
+                    <input id="_fixedNavbar" type="checkbox" checked>
+                    <span class="input-span"></span>Fixed navbar</label>
+                <label class="ui-checkbox ui-checkbox-gray">
+                    <input id="_fixedlayout" type="checkbox">
+                    <span class="input-span"></span>Fixed layout</label>
+                <label class="ui-checkbox ui-checkbox-gray">
+                    <input class="js-sidebar-toggler" type="checkbox">
+                    <span class="input-span"></span>Collapse sidebar</label>
+            </div>
+            <div class="font-strong">LAYOUT STYLE</div>
+            <div class="m-t-10">
+                <label class="ui-radio ui-radio-gray m-r-10">
+                    <input type="radio" name="layout-style" value="" checked="">
+                    <span class="input-span"></span>Fluid</label>
+                <label class="ui-radio ui-radio-gray">
+                    <input type="radio" name="layout-style" value="1">
+                    <span class="input-span"></span>Boxed</label>
+            </div>
+        </div>
+    </div>
+    <!-- END THEME CONFIG PANEL-->
+    <!-- BEGIN PAGA BACKDROPS-->
+    <div class="sidenav-backdrop backdrop"></div>
+    <div class="preloader-backdrop">
+        <div class="page-preloader">Loading</div>
+    </div>
+    <!-- END PAGA BACKDROPS-->
+    <!-- CORE PLUGINS-->
+    <script src="/admin/vendors/jquery/dist/jquery.min.js" type="text/javascript"></script>
+    <script src="/admin/vendors/popper.js/dist/umd/popper.min.js" type="text/javascript"></script>
+    <script src="/admin/vendors/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="/admin/vendors/metisMenu/dist/metisMenu.min.js" type="text/javascript"></script>
+    <script src="/admin/vendors/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+    <!-- PAGE LEVEL PLUGINS-->
+    <script src="/admin/vendors/chart.js/dist/Chart.min.js" type="text/javascript"></script>
+    <script src="/admin/vendors/jvectormap/jquery-jvectormap-2.0.3.min.js" type="text/javascript"></script>
+    <script src="/admin/vendors/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
+    <script src="/admin/vendors/jvectormap/jquery-jvectormap-us-aea-en.js" type="text/javascript"></script>
+    <!-- CORE SCRIPTS-->
+    <script src="/admin/js/app.min.js" type="text/javascript"></script>
+    <!-- PAGE LEVEL SCRIPTS-->
+    <script src="/admin/js/scripts/dashboard_1_demo.js" type="text/javascript"></script>
 
-        new Chart(ctx, {
-            type: "bar",
-            data: {
-                labels: ["M", "T", "W", "T", "F", "S", "S"],
-                datasets: [{
-                    label: "Sales",
-                    tension: 0.4,
-                    borderWidth: 0,
-                    borderRadius: 4,
-                    borderSkipped: false,
-                    backgroundColor: "rgba(255, 255, 255, .8)",
-                    data: [50, 20, 10, 22, 50, 10, 40],
-                    maxBarThickness: 6
-                }, ],
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    legend: {
-                        display: false,
-                    }
-                },
-                interaction: {
-                    intersect: false,
-                    mode: 'index',
-                },
-                scales: {
-                    y: {
-                        grid: {
-                            drawBorder: false,
-                            display: true,
-                            drawOnChartArea: true,
-                            drawTicks: false,
-                            borderDash: [5, 5],
-                            color: 'rgba(255, 255, 255, .2)'
-                        },
-                        ticks: {
-                            suggestedMin: 0,
-                            suggestedMax: 500,
-                            beginAtZero: true,
-                            padding: 10,
-                            font: {
-                                size: 14,
-                                weight: 300,
-                                family: "Roboto",
-                                style: 'normal',
-                                lineHeight: 2
-                            },
-                            color: "#fff"
-                        },
-                    },
-                    x: {
-                        grid: {
-                            drawBorder: false,
-                            display: true,
-                            drawOnChartArea: true,
-                            drawTicks: false,
-                            borderDash: [5, 5],
-                            color: 'rgba(255, 255, 255, .2)'
-                        },
-                        ticks: {
-                            display: true,
-                            color: '#f8f9fa',
-                            padding: 10,
-                            font: {
-                                size: 14,
-                                weight: 300,
-                                family: "Roboto",
-                                style: 'normal',
-                                lineHeight: 2
-                            },
-                        }
-                    },
-                },
-            },
-        });
-
-
-        var ctx2 = document.getElementById("chart-line").getContext("2d");
-
-        new Chart(ctx2, {
-            type: "line",
-            data: {
-                labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                datasets: [{
-                    label: "Mobile apps",
-                    tension: 0,
-                    borderWidth: 0,
-                    pointRadius: 5,
-                    pointBackgroundColor: "rgba(255, 255, 255, .8)",
-                    pointBorderColor: "transparent",
-                    borderColor: "rgba(255, 255, 255, .8)",
-                    borderColor: "rgba(255, 255, 255, .8)",
-                    borderWidth: 4,
-                    backgroundColor: "transparent",
-                    fill: true,
-                    data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
-                    maxBarThickness: 6
-
-                }],
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    legend: {
-                        display: false,
-                    }
-                },
-                interaction: {
-                    intersect: false,
-                    mode: 'index',
-                },
-                scales: {
-                    y: {
-                        grid: {
-                            drawBorder: false,
-                            display: true,
-                            drawOnChartArea: true,
-                            drawTicks: false,
-                            borderDash: [5, 5],
-                            color: 'rgba(255, 255, 255, .2)'
-                        },
-                        ticks: {
-                            display: true,
-                            color: '#f8f9fa',
-                            padding: 10,
-                            font: {
-                                size: 14,
-                                weight: 300,
-                                family: "Roboto",
-                                style: 'normal',
-                                lineHeight: 2
-                            },
-                        }
-                    },
-                    x: {
-                        grid: {
-                            drawBorder: false,
-                            display: false,
-                            drawOnChartArea: false,
-                            drawTicks: false,
-                            borderDash: [5, 5]
-                        },
-                        ticks: {
-                            display: true,
-                            color: '#f8f9fa',
-                            padding: 10,
-                            font: {
-                                size: 14,
-                                weight: 300,
-                                family: "Roboto",
-                                style: 'normal',
-                                lineHeight: 2
-                            },
-                        }
-                    },
-                },
-            },
-        });
-
-        var ctx3 = document.getElementById("chart-line-tasks").getContext("2d");
-
-        new Chart(ctx3, {
-            type: "line",
-            data: {
-                labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                datasets: [{
-                    label: "Mobile apps",
-                    tension: 0,
-                    borderWidth: 0,
-                    pointRadius: 5,
-                    pointBackgroundColor: "rgba(255, 255, 255, .8)",
-                    pointBorderColor: "transparent",
-                    borderColor: "rgba(255, 255, 255, .8)",
-                    borderWidth: 4,
-                    backgroundColor: "transparent",
-                    fill: true,
-                    data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
-                    maxBarThickness: 6
-
-                }],
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    legend: {
-                        display: false,
-                    }
-                },
-                interaction: {
-                    intersect: false,
-                    mode: 'index',
-                },
-                scales: {
-                    y: {
-                        grid: {
-                            drawBorder: false,
-                            display: true,
-                            drawOnChartArea: true,
-                            drawTicks: false,
-                            borderDash: [5, 5],
-                            color: 'rgba(255, 255, 255, .2)'
-                        },
-                        ticks: {
-                            display: true,
-                            padding: 10,
-                            color: '#f8f9fa',
-                            font: {
-                                size: 14,
-                                weight: 300,
-                                family: "Roboto",
-                                style: 'normal',
-                                lineHeight: 2
-                            },
-                        }
-                    },
-                    x: {
-                        grid: {
-                            drawBorder: false,
-                            display: false,
-                            drawOnChartArea: false,
-                            drawTicks: false,
-                            borderDash: [5, 5]
-                        },
-                        ticks: {
-                            display: true,
-                            color: '#f8f9fa',
-                            padding: 10,
-                            font: {
-                                size: 14,
-                                weight: 300,
-                                family: "Roboto",
-                                style: 'normal',
-                                lineHeight: 2
-                            },
-                        }
-                    },
-                },
-            },
-        });
-    </script>
-    <script>
-        var win = navigator.platform.indexOf('Win') > -1;
-        if (win && document.querySelector('#sidenav-scrollbar')) {
-            var options = {
-                damping: '0.5'
-            }
-            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-        }
-    </script>
-    <!-- Github buttons -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="/admin/js/material-dashboard.min.js?v=3.0.0"></script>
 </body>
 
 </html>
--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -1,25 +1,192 @@
-<aside
-    class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark"
-    id="sidenav-main">
-    <div class="sidenav-header">
-        <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-            aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0">
-            <img src="/admin/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-1 font-weight-bold text-white">Dashboard</span>
-        </a>
-    </div>
-    <hr class="horizontal light mt-0 mb-2">
-    <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link text-white {{ Request::is('dashboard') ? 'active' : '' }}" href="/dashboard">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="material-icons opacity-10">dashboard</i>
-                    </div>
-                    <span class="nav-link-text ms-1">Dashboard</span>
+<!-- START SIDEBAR-->
+<nav class="page-sidebar" id="sidebar">
+    <div id="sidebar-collapse">
+        <div class="admin-block d-flex">
+            <div>
+                <img src="/admin/img/admin-avatar.png" width="45px" />
+            </div>
+            <div class="admin-info">
+                <div class="font-strong">James Brown</div><small>Administrator</small>
+            </div>
+        </div>
+        <ul class="side-menu metismenu">
+            <li>
+                <a class="active" href="index.html"><i class="sidebar-item-icon fa fa-th-large"></i>
+                    <span class="nav-label">Dashboard</span>
                 </a>
+            </li>
+            <li class="heading">FEATURES</li>
+            <li>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-bookmark"></i>
+                    <span class="nav-label">Basic UI</span><i class="fa fa-angle-left arrow"></i></a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="colors.html">Colors</a>
+                    </li>
+                    <li>
+                        <a href="typography.html">Typography</a>
+                    </li>
+                    <li>
+                        <a href="panels.html">Panels</a>
+                    </li>
+                    <li>
+                        <a href="buttons.html">Buttons</a>
+                    </li>
+                    <li>
+                        <a href="tabs.html">Tabs</a>
+                    </li>
+                    <li>
+                        <a href="alerts_tooltips.html">Alerts &amp; Tooltips</a>
+                    </li>
+                    <li>
+                        <a href="badges_progress.html">Badges &amp; Progress</a>
+                    </li>
+                    <li>
+                        <a href="lists.html">List</a>
+                    </li>
+                    <li>
+                        <a href="cards.html">Card</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-edit"></i>
+                    <span class="nav-label">Forms</span><i class="fa fa-angle-left arrow"></i></a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="form_basic.html">Basic Forms</a>
+                    </li>
+                    <li>
+                        <a href="form_advanced.html">Advanced Plugins</a>
+                    </li>
+                    <li>
+                        <a href="form_masks.html">Form input masks</a>
+                    </li>
+                    <li>
+                        <a href="form_validation.html">Form Validation</a>
+                    </li>
+                    <li>
+                        <a href="text_editors.html">Text Editors</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-table"></i>
+                    <span class="nav-label">Tables</span><i class="fa fa-angle-left arrow"></i></a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="table_basic.html">Basic Tables</a>
+                    </li>
+                    <li>
+                        <a href="datatables.html">Datatables</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-bar-chart"></i>
+                    <span class="nav-label">Charts</span><i class="fa fa-angle-left arrow"></i></a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="charts_flot.html">Flot Charts</a>
+                    </li>
+                    <li>
+                        <a href="charts_morris.html">Morris Charts</a>
+                    </li>
+                    <li>
+                        <a href="chartjs.html">Chart.js</a>
+                    </li>
+                    <li>
+                        <a href="charts_sparkline.html">Sparkline Charts</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-map"></i>
+                    <span class="nav-label">Maps</span><i class="fa fa-angle-left arrow"></i></a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="maps_vector.html">Vector maps</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="icons.html"><i class="sidebar-item-icon fa fa-smile-o"></i>
+                    <span class="nav-label">Icons</span>
+                </a>
+            </li>
+            <li class="heading">PAGES</li>
+            <li>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-envelope"></i>
+                    <span class="nav-label">Mailbox</span><i class="fa fa-angle-left arrow"></i></a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="mailbox.html">Inbox</a>
+                    </li>
+                    <li>
+                        <a href="mail_view.html">Mail view</a>
+                    </li>
+                    <li>
+                        <a href="mail_compose.html">Compose mail</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="calendar.html"><i class="sidebar-item-icon fa fa-calendar"></i>
+                    <span class="nav-label">Calendar</span>
+                </a>
+            </li>
+            <li>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-file-text"></i>
+                    <span class="nav-label">Pages</span><i class="fa fa-angle-left arrow"></i></a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="invoice.html">Invoice</a>
+                    </li>
+                    <li>
+                        <a href="profile.html">Profile</a>
+                    </li>
+                    <li>
+                        <a href="login.html">Login</a>
+                    </li>
+                    <li>
+                        <a href="register.html">Register</a>
+                    </li>
+                    <li>
+                        <a href="lockscreen.html">Lockscreen</a>
+                    </li>
+                    <li>
+                        <a href="forgot_password.html">Forgot password</a>
+                    </li>
+                    <li>
+                        <a href="error_404.html">404 error</a>
+                    </li>
+                    <li>
+                        <a href="error_500.html">500 error</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-sitemap"></i>
+                    <span class="nav-label">Menu Levels</span><i class="fa fa-angle-left arrow"></i></a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="javascript:;">Level 2</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;">
+                            <span class="nav-label">Level 2</span><i class="fa fa-angle-left arrow"></i></a>
+                        <ul class="nav-3-level collapse">
+                            <li>
+                                <a href="javascript:;">Level 3</a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">Level 3</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </li>
         </ul>
     </div>
-</aside>
+</nav>
+<!-- END SIDEBAR-->
